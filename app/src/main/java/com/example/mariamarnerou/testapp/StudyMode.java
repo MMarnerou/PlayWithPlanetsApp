3900649e798b8dcac90fb21efe1fa382f03686d0
mmarnerou <maria.marnerou1992@hotmail.com>
2018-04-02 22:21:06 +0300

    embedded html in StudyMody activity. Correcting Text

--- app/src/main/java/com/example/mariamarnerou/testapp/StudyMode.java
+++ app/src/main/java/com/example/mariamarnerou/testapp/StudyMode.java
@@ -1,36 +1,35 @@
 package com.example.mariamarnerou.testapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-
-import java.io.IOException;
-import java.io.InputStream;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 public class StudyMode extends AppCompatActivity {
-    TextView bookContent;
+    public String fileName = "planites.html";
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_mode);
 
-        //Start Slide Page
-        bookContent = (TextView) findViewById(R.id.bookContent);
-        try {
-            InputStream book = getAssets().open("Quiz1.txt");
-            int size = book.available();
-
-            //Read the entire asset into a local byte buffer
-            byte[] buffer = new byte[size];
-            book.read(buffer);
-            book.close();
-
-            String text = new String(buffer);
-            bookContent.setText(text);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        //End Slide Page
+        webView = findViewById(R.id.bookContent);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/" + fileName);
+//        try {
+//            InputStream book = getAssets().open("Quiz1.txt");
+//            int size = book.available();
+//
+//            //Read the entire asset into a local byte buffer
+//            byte[] buffer = new byte[size];
+//            book.read(buffer);
+//            book.close();
+//
+//            String text = new String(buffer);
+//            bookContent.setText(text);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        //End Slide Page
     }
 }
